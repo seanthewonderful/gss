07e97ec98b3cf9b6334557719771775220afdd79
Sean Fagan <58955260+seanthewonderful@users.noreply.github.com>
2022-05-10 11:02:23 -0600

    api requests

--- demo.py
+++ demo.py
@@ -1,12 +1,17 @@
 import requests
 
-endpoint = "https://api.twitter.com/2/users/1358218911594864640/liked_tweets"
+endpoint = "https://api.twitter.com/2/users/:id/liked_tweets"
+
+headers = {
+    "access_token": "PbCpzCP5aVq39GembNWSAo89V",
+    "token_secret": "aKOYl7fP4lQauA9yKDaBrC67Kl8d2SYiVtXnY4O3luC5FvurX1",
+    "bearer_token": "Bearer AAAAAAAAAAAAAAAAAAAAAGefcQEAAAAAWtdc9ZAkIjTgWx%2FP7l7h4KXV%2BX8%3DsEfZdl5ZeZtEaf8om1JPhy51PAJYaEpyuGf1TkAHybiPNAPmGG"
+}
 
 params = {
-    "Authorization": "Bearer AAAAAAAAAAAAAAAAAAAAAGefcQEAAAAAWtdc9ZAkIjTgWx%2FP7l7h4KXV%2BX8%3DsEfZdl5ZeZtEaf8om1JPhy51PAJYaEpyuGf1TkAHybiPNAPmGG",
-    # "secret_key": "aKOYl7fP4lQauA9yKDaBrC67Kl8d2SYiVtXnY4O3luC5FvurX1"
+    'id': "2620199928"
 }
-# print(response)
 
-response = requests.get(url=endpoint, params=params)
+
+response = requests.get(url=endpoint, params=params, headers=headers)
 print(response.json())
